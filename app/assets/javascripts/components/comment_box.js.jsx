8f58945c40e1cf362a2f7424a5789d42573da49a
kokoriru <yuka-ogihara@hitotomirai.co.jp>
2018-06-01 12:05:26 +0900

    Add a child Component to comment_box.js.jsx.

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -2,7 +2,29 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world: I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="CommentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="CommentForm">
+        Hello, world! I am a CommentForm.
       </div>
     );
   }
